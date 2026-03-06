0c5472f86d1ca65417a71969ddd529fbe33c77b0
mousum <62622428+Mousum2002@users.noreply.github.com>
2026-03-06 22:39:31 +0100

    monkey patching was not working properly after the data arrived even though the variable ref chenged, had to use manual changedetector

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { CreateTask } from './create-task/create-task';
 import { CommonModule } from '@angular/common';
 import { Task } from '../Model/Task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 
 @Component({
@@ -11,10 +12,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
    showCreateTaskForm: boolean = false;
    http:HttpClient = inject(HttpClient);
-   
+   allTasks: Task[] = [];
+   cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+   ngOnInit(){
+    this.fetchTasks();
+   }
+
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -30,6 +37,24 @@ export class Dashboard {
     this.http.post<{name:string}>('https://angularhttpclient-c80a8-default-rtdb.firebaseio.com/tasks.json',data,
       {headers: header}).subscribe((response)=>{
       console.log(response);
+      
+    });
+  }
+
+  private fetchTasks(){
+    this.http.get<{[key:string]: Task}>('https://angularhttpclient-c80a8-default-rtdb.firebaseio.com/tasks.json').pipe(map((response)=>{
+      let tasks = [];
+      for(let key in response){
+        if(response.hasOwnProperty(key)){
+        tasks.push({...response[key], id: key})}
+      }
+      return tasks;
+    })).subscribe
+    ((tasks)=>{
+      this.allTasks = tasks;
+      this.cdr.detectChanges();
+      console.log(this.allTasks.map(t => t.id))
+      console.log(this.allTasks.length);
     });
   }
 
