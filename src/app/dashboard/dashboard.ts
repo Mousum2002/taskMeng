29418091cb0e89cb065a33fc4f04b542ea30ec96
mousum <62622428+Mousum2002@users.noreply.github.com>
2026-03-06 17:48:36 +0100

    integrated google firebase without security/ test setup

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CreateTask } from './create-task/create-task';
 import { CommonModule } from '@angular/common';
 import { Task } from '../Model/Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
 @Component({
@@ -12,6 +13,8 @@ import { Task } from '../Model/Task';
 })
 export class Dashboard {
    showCreateTaskForm: boolean = false;
+   http:HttpClient = inject(HttpClient);
+   
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -21,7 +24,13 @@ export class Dashboard {
     this.showCreateTaskForm = false;
   }
   CreateTask(data: Task){
-    console.log(data);
+    const header = new HttpHeaders({
+      'MyHeader': 'hello world'
+    });
+    this.http.post<{name:string}>('https://angularhttpclient-c80a8-default-rtdb.firebaseio.com/tasks.json',data,
+      {headers: header}).subscribe((response)=>{
+      console.log(response);
+    });
   }
 
 }
